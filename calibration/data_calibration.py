fb9ddeac1e208443ab4d256743a58e213ac81186
Angelo B. <81601652+abarbierif@users.noreply.github.com>
2025-07-02 16:02:17 -0400

    [MAINTENANCE]: Delete calibration directory

--- calibration/data_calibration.py
+++
@@ -1,7 +0,0 @@
-
-CALIBRATION_DATA = {
-    "DWDM": {"osa":"DWDM_r1.0_s50", "w0sa":"w0saDWDM_ri1.0_si50_ro1.0_so50"},
-    "CWDM": {"osa":"CWDM_r1.0_s50", "w0sa":"w0saCWDM_ri1.0_si50_ro1.0_so50"},
-    # future entries
-}
-
